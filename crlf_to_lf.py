bbe0cc2bda2d99b92528d29fe41a082e76784c84
sakurafisch <sakurafisch@gmail.com>
2022-08-31 15:39:47 +0800

    feat: set vscode default to lf eol

--- crlf_to_lf.py
+++ crlf_to_lf.py
@@ -7,7 +7,7 @@ script_file = __file__
 
 files: list[str] = os.listdir()
 
-ignores: list[str] = ["crlf_to_lf.py", ".git", "node_modules", "dist"]
+ignores: list[str] = ["crlf_to_lf.py", ".git", "node_modules", "dist", "yarn-error.log"]
 
 for ignore in ignores:
     if ignore in files:
